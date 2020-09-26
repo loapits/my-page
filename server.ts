3c693d9f6939d75b4466ff1bb6c033f0d9372d7f
loapits <gera59377@gmail.com>
2020-09-27 01:38:50 +0600

    switch base url in api file and setting node

--- server.ts
+++ server.ts
@@ -1,22 +1,19 @@
 const express = require('express')
-//  const favicon = require('express-favicon');
- const path = require('path')
- const port = process.env.PORT || 8080
- 
- // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
- const app = express()
-//  app.use(favicon(__dirname + '/build/favicon.png')); 
- 
-//здесь наше приложение отдаёт статику
+const path = require('path')
+const port = process.env.PORT || 8080
+
+const app = express()
+
+app.use((req: any, res: any, next: any) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
+
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 
-//простой тест сервера
-app.get('/ping', (req, res) => {
-  return res.send('pong');
-})
-
-//обслуживание html
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
