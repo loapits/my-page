dfb5a3029f76059880da0776e8de85757ed6d9cc
loapits <gera59377@gmail.com>
2020-09-27 01:43:54 +0600

    switch base url in api file and setting node

--- server.ts
+++ server.ts
@@ -4,15 +4,12 @@ const port = process.env.PORT || 8080
 
 const app = express()
 
-app.use((req: any, res: any, next: any) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
-  res.header("Access-Control-Allow-Headers", "Content-Type")
-  next()
-})
-
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/ping', (req, res) => {
+  return res.send('pong')
+})
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
